02a151b152e12fa0849ee48c1828ff5414d45a36
AdvellerdC <84400051+AdvellerdC@users.noreply.github.com>
2021-10-28 22:19:52 +0400

    Add project files.

---
+++ Send-Mail/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+namespace Send_Mail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                var client = new SmtpClient("smtp.gmail.com", 587)
+                {
+                    Credentials = new NetworkCredential(textBox1.Text, textBox2.Text),
+                    EnableSsl = true
+                };
+                client.Send(textBox1.Text, textBox3.Text, textBox4.Text, textBox5.Text);
+                MessageBox.Show("Mail has been sent successfully!", "Success!");
+            }
+            catch
+            {
+                MessageBox.Show("An error has occured while we sent gmail or you didn't fill these correctly", "An error has occured");
+            }
+        }
+    }
+}
